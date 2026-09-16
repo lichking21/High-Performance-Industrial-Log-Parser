58d4b534c6aff1f1a20052c1de15f29ccbb39cb1
lichking21 <kazakovaman@gmail.com>
2026-09-16 15:57:28 +0200

    (new) --gen, --parse flags add

--- src/main.c
+++ src/main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <time.h>
 #include "generator.h"
+#include "parser.h"
 
 size_t convert_string(const char* str)
 {
@@ -33,45 +34,53 @@ int main(int argc, char** argv)
 {
     srand(time(NULL));
 
-    if (argc < 5)
-    {
-        printf("(ERR) >> Not enough arguments\n");
-        return -1;
-    }
-    else if (argc > 5)
-    {
-        printf("(ERR) >> Too many arguments\n");
-        return -1;
-    }
-
     char* flag = argv[1];
-    size_t target_size = convert_string(argv[2]);
-    char* size_unit = argv[3];
-    char* file_name = argv[4];
+    char* file_name = argv[2];
 
-    printf("(LOG) >> %s\n", flag);
-    printf("(LOG) >> %ld\n", target_size);
-    printf("(LOG) >> %s\n", size_unit);
-    printf("(LOG) >> %s\n", file_name);
-
-    if (strcmp(flag, "--gen") == 0)
+    if (argc > 0 && argc <= 3)
     {
-        if (strcmp(size_unit, "B") == 0)         target_size *= 1;
-        else if (strcmp(size_unit, "KB") == 0)   target_size *= 1024;
-        else if (strcmp(size_unit, "MB") == 0)   target_size *= 1024 * 1024;
-        else if (strcmp(size_unit, "GB") == 0)   target_size *= 1024 * 1024 * 1024;
+        if (strcmp(flag, "--parse") == 0)
+        {
+            parse_file(file_name);
+        }
         else
         {
-            printf("(ERR) >> Invalid size unit.\n Try to use B, KB, MB, GB\n");
+            printf("(ERR) >> Invalid flag.\n Try to use: \n");
+            printf("\t--parse - parse file <file_name>\n");
             return -1;
         }
+    }
+    else if (argc > 3)
+    {
+        char* flag = argv[1];
+        char* file_name = argv[2];
+        size_t target_size = convert_string(argv[3]);
+        char* size_unit = argv[4];
 
-        generate_csv(file_name, target_size);
+        if (strcmp(flag, "--gen") == 0)
+        {
+            if (strcmp(size_unit, "B") == 0)         target_size *= 1;
+            else if (strcmp(size_unit, "KB") == 0)   target_size *= 1024;
+            else if (strcmp(size_unit, "MB") == 0)   target_size *= 1024 * 1024;
+            else if (strcmp(size_unit, "GB") == 0)   target_size *= 1024 * 1024 * 1024;
+            else
+            {
+                printf("(ERR) >> Invalid size unit.\n Try to use B, KB, MB, GB\n");
+                return -1;
+            }
+
+            generate_csv(file_name, target_size);
+        }
+        else
+        {
+            printf("(ERR) >> Invalid flag.\n Try to use: \n");
+            printf("\t--gen - generate file <file_name> <file_size> <size_unit>\n");
+            return -1;
+        }
     }
     else
     {
-        printf("(ERR) >> Invalid flag.\n Try to use: \n");
-        printf("\t--gen - generate file <file_size> <size_unit> <file_name>\n");
+        printf("(ERR) >> Not enough arguments: %d\n", argc);
         return -1;
     }
 
