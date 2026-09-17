97627e1f716f92e6707b601f0037810debd16cce
lichking21 <kazakovaman@gmail.com>
2026-09-17 15:53:38 +0200

    (new) parse_type/status funcs definition

--- include/parser.h
+++ include/parser.h
@@ -4,6 +4,9 @@
 #include "data.h"
 
 int parse_file(const char* filename);
+
+Type parse_type(size_t len, char* str);
+Status parse_status(size_t len, char* str);
 int parse_line(char* line, Data* data);
 
 #endif
