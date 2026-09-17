7d34b08026e845438bf2b42f6465d15c54e9b4e8
lichking21 <kazakovaman@gmail.com>
2026-09-17 09:39:48 +0200

    (new) file stats calculation add

--- src/parser.c
+++ src/parser.c
@@ -1,8 +1,10 @@
 #include "parser.h"
 #include "data.h"
+#include "stats.h"
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <time.h>
 
 #define MAXBUFF 1000
 
@@ -17,19 +19,47 @@ int parse_file(const char* filename)
 
     char line_buff[MAXBUFF];
 
-    // skip first line with columns names
-    fgets(line_buff, MAXBUFF, fp);
+    Stats stats;
+    size_t records = 0, corrupted = 0;
 
+    struct timespec start, end;
+    double processing_time = 0, throughput = 0;
+
+    // get file size
+    fseek(fp, 0, SEEK_END);
+    long size = ftell(fp);
+    rewind(fp);
+
+    // skip header
+    if (fgets(line_buff, MAXBUFF, fp) == NULL)
+    {
+        fclose(fp);
+        return -1;
+    }
+
+    // initialize Stats struct
+    stats_init(&stats);
+
+    // mark the time
+    timespec_get(&start, TIME_UTC);
+
+    // parse file
     while(fgets(line_buff, MAXBUFF, fp) != NULL)
     {
         Data data;
+        records += 1;
 
         if (parse_line(line_buff, &data) != 0)
         {
-            printf("(WARN) >> Corrupted line\n");
+            corrupted += 1;
             continue;
         }
+
+        if (stats_upd(&stats, &data, records, corrupted) != 0)
+            continue;
     }
+
+    timespec_get(&end, TIME_UTC);
 
     if (ferror(fp))
     {
@@ -38,8 +68,13 @@ int parse_file(const char* filename)
         return -1;
     }
 
+    processing_time = (double)(end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / 1e9;
+    throughput = ((double)size / (1024.0 * 1024.0)) / processing_time;
+    upd_time(&stats, processing_time, throughput);
 
     printf(">> File successfully parsed!\n");
+    stats_print(&stats);
+
     fclose(fp);
     return 0;
 }
